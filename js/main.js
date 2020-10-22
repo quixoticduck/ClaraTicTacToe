6a3b45d4c29863d5e57d83cf9134c0c1bcc8f0dd
Clara <quixoticduck.com>
2020-10-22 21:30:16 +0100

    basically everything whoops

--- js/main.js
+++ js/main.js
@@ -2,6 +2,17 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 
 // constants
 
+const winningCombos = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+];
+
 // app's state (variables)
 let board;
 //declaring a function
@@ -16,16 +27,29 @@ function init() {
     };
 
 let turn = 'X';
+let win;
 
 // cached element references
 //array.from takes an array-like object and turns it into an actual array so that we can do stuff like .forEach to it
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages =document.querySelector('h2');
 
 // event listeners
 //finds the first element named board (we only have one called board)
 document.getElementById('board').addEventListener('click', handleTurn);
 
 // functions
+
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo,index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board [combo[0]] === board [combo[2]]) {
+            winner = board[combo[0]];
+        }
+    });
+    return winner;
+}
+
 function render() {
     //again you can name them anything but first is contents of each array item, second is which number in the array and last would be the array itself (rarely used)
     board.forEach(function(mark,index) {
@@ -35,7 +59,9 @@ function render() {
         squares[index].textContent = mark;
         //squares is an array, the [] is how we access an element of an array based on its position
     });
+
 };
+
 
 //remember to call the init function
 //the letters init could be anything, use something descriptive that will make sense later
@@ -51,4 +77,45 @@ function handleTurn(event) {
     //square brackets are usually to do with arrays/or grabbing some info from the thing on the left
     board[idx] = turn;
     console.log(board);
-};
+
+    win = getWinner();
+    render();
+
+
+    // turn === 'X' ? turn = 'O' : turn = 'X';
+
+    // ternary version of if statement    
+    // turn === 'X' ? turn = 'O' : turn = 'X';
+
+    // {/* if (turn === 'X') {
+    //     turn = 'O'
+    //     } else {
+    //     turn = 'X'
+    //     }; */}
+
+    //refactored version to make an efficient ternary
+    turn = turn === 'X' ?  'O' :  'X';
+        // if you want to put a variable within a string you have to use a ` instead of a ' quote
+        //if win is true, then do the thing after ?, and then the : means else
+    messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+
+    //way to write the above without ternary
+    // if (win) {
+    //     messages.textContent = `${win} wins the game!`;
+    //   } else {
+    //     messages.textContent = `It's ${turn}'s turn!`;
+    //   }
+    console.log(messages);
+    console.log(turn);
+
+    // if the first square is not empty, and if the contents of the first square is equal to the contents of the second square and if the contents of the first square is equal to the last square
+    // then win is equal to whatever is in the first square, else there is no win 
+    // if (board[0] === true && board[0] === board [1] && board [0] === board [2]) {
+    //         winner = board[0]; 
+    //     }
+    //     else {
+    //         win = null;
+    //     }
+
+
+    }
